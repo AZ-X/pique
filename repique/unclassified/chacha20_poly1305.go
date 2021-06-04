e86d6ef9e3a57249b155384fc3d45b0fcaed3221
AZ-X <64643648+AZ-X@users.noreply.github.com>
2021-06-04 20:40:00 +0800

    trivial correction
    (cherry picked from commit 0c02920f640ce83c0abe60cd4d3726d6432da5c0)

--- repique/unclassified/chacha20_poly1305.go
+++ repique/unclassified/chacha20_poly1305.go
@@ -27,10 +27,10 @@ func chacha20_NewUnauthenticatedCipher(key, nonce []byte) (*Cipher, error)
 // Splitter
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//go:linkname poly1305_TagSize vendor/golang.org/x/crypto/chacha20.TagSize
+//go:linkname poly1305_TagSize vendor/golang.org/x/crypto/poly1305.TagSize
 const poly1305_TagSize = 16
 
-//go:linkname MAC vendor/golang.org/x/crypto/chacha20.MAC
+//go:linkname MAC vendor/golang.org/x/crypto/poly1305.MAC
 type MAC struct {}
 
 //go:linkname (*MAC).Write vendor/golang.org/x/crypto/poly1305.(*MAC).Write
@@ -45,8 +45,8 @@ func (h *MAC) Verify(expected []byte) bool
 //go:linkname poly1305_New vendor/golang.org/x/crypto/poly1305.New
 func poly1305_New(key *[32]byte) *MAC
 
-////go:linkname poly1305_Sum vendor/golang.org/x/crypto/poly1305.Sum 
+////go:linkname poly1305_Sum vendor/golang.org/x/crypto/poly1305.Sum
 //func poly1305_Sum(out *[16]byte, m []byte, key *[32]byte)
 //
-////go:linkname poly1305_Verify vendor/golang.org/x/crypto/poly1305.Verify 
+////go:linkname poly1305_Verify vendor/golang.org/x/crypto/poly1305.Verify
 //func poly1305_Verify(mac *[16]byte, m []byte, key *[32]byte) bool
